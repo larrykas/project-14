afe76012d20b0d37d4321cca854a04a98578eeb5
larrykas <larrykas@users.noreply.github.com>
2013-11-27 21:16:43 +0300

    Update atm_test.rb

--- atm_test.rb
+++ atm_test.rb
@@ -1,9 +1,6 @@
-
 # This is the first commit for this project
 require 'test/unit'
 class Account < Test::Unit::TestCase
-=======
-class Account
 
   def initialize(balance, name, account_number) 
     @balance = balance
@@ -12,41 +9,29 @@ class Account
     
   end
 
-   def deposit(amount)
-     assert_raise @balance += amount
-   end
-
-   def withdraw(amount)
-    assert_raise @balance -= amount
-=======
+  def deposit(amount)
+    assert_raise @balance += amount
   end
 
-   def deposit(amount)
-     @balance += amount
-   end
+  def withdraw(amount)
+    assert_raise @balance -= amount
+  end
 
-   def withdraw(amount)
-     @balance -= amount
-
-   end
  
-   def balance
-     puts "Name: " + @name
-     puts "Account number: " + @account_number.to_s 
-     puts "Balance: " + @balance.to_s
-   end
+  def balance
+    puts "Name: " + @name
+    puts "Account number: " + @account_number.to_s 
+    puts "Balance: " + @balance.to_s
+  end
 
-   def transfer(amount, target_account)
+  def transfer(amount, target_account)
 
-     assert_raise @balance -= amount
-     assert_raise target_account.deposit(amount)
-=======
-     @balance -= amount
-     target_account.deposit(amount)
+    assert_raise @balance -= amount
+    assert_raise target_account.deposit(amount)
 
-   end
+  end
 
-   def status
-     return @balance
-   end
+  def status
+    return @balance
+  end
  end
